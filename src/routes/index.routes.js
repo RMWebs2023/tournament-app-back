845aa3877f1b1f638f49bd5bb5011539b8c52f32
René <107271569+renevalderrey@users.noreply.github.com>
2024-09-12 20:14:18 -0300

    agregué ruta putPlayer

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const getPlayer = require("./routesPlayer/getPlayer.routes.js");
 const postPlayer = require("./routesPlayer/postPlayer.routes.js");
+const putPlayer = require("./routesPlayer/putPlayer.routes.js");
 const deletePlayer = require("./routesPlayer/deletePlayer.routes.js");
 
 const router = Router();
 
 router.use("/", getPlayer);
 router.use("/", postPlayer);
+router.use("/", putPlayer);
 router.use("/", deletePlayer);
 
 module.exports = router;
---
+++ src/routes/routesPlayer/putPlayer.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const Player = require("../../models/Player.js");
+
+const router = Router();
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await Player.findByIdAndUpdate(id);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
